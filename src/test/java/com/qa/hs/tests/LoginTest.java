e2c1b7572ff284817aef81815a3238246524ae6a
abhilash0675qa@gmail.com <naveen.sekharamahanthi@gmail.com>
2021-08-28 13:48:52 +0530

    naveen recent 28 AUG

--- src/test/java/com/qa/hs/tests/LoginTest.java
+++ src/test/java/com/qa/hs/tests/LoginTest.java
@@ -1,14 +1,15 @@
 package com.qa.hs.tests;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.qa.hs.keyword.base.*;
+//import com.qa.hs.keyword.base.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -16,8 +17,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-
+import com.qa.hs.keyword.base.Base;
 import com.qa.hs.keyword.engine.KeyWordEngine;
 /**
  * 
@@ -30,15 +30,18 @@ public class LoginTest {
 	public Base base;
 	public Properties prop;
 	public static WebDriver driver;
-//	public ExtentReports report;
+	public static Logger log;
+	public ExtentReports report;
 
 	
 	@BeforeTest
 	public void start() {
 		base = new Base();
 		prop = base.init_properties();
+		log = LogManager.getLogger(LoginTest.class.getName());
 		driver = base.init_driver(prop.getProperty("browser"));
-		System.out.println("===DRIVER in BEFORE TEST ===="+driver);
+		log.debug("driver initiated");
+//		System.out.println("===DRIVER in BEFORE TEST ===="+driver);
 //		String path = System.getProperty("user.dir")+"/reports/index.html";
 //		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 //		reporter.config().setReportName("Avesdo Automation Report");
@@ -46,9 +49,12 @@ public class LoginTest {
 //		report = new ExtentReports();
 //		report.attachReporter(reporter);
 		driver.manage().deleteAllCookies();
+		log.info("cookies deleted");
 		driver.manage().window().maximize();
+		log.error("window maximized");
 		driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS);
 		driver.manage().timeouts().setScriptTimeout(300, TimeUnit.SECONDS);
+		log.fatal("initialization completed");
 		
 	}
 	
